50348c6ed3b73f563a4ef57c9b50fff795d00867
Josh_Frey <kalumba81@googlemail.com>
2015-10-31 02:36:43 +0100

    class added

---
+++ NWAT_SSA_WS_15_16_Test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWAT_SSA_WS_15_16_Test
+{
+    class Class1
+    {
+    }
+}
