deff5553e638fd62615782c829d92190e6e721d6
Josh_Frey <kalumba81@googlemail.com>
2015-10-31 02:34:59 +0100

    initial Test

---
+++ NWAT_SSA_WS_15_16_Test/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NWAT_SSA_WS_15_16_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
